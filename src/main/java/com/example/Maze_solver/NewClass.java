eb3424dd8583a49ce634a9a16419a4d06ba10639
VinathaSree <98879376+VinathaSree@users.noreply.github.com>
2023-08-26 17:46:39 +0530

    Add logic for tracing the path using DFS

--- src/main/java/com/example/Maze_solver/NewClass.java
+++ src/main/java/com/example/Maze_solver/NewClass.java
@@ -23,6 +23,7 @@ public class NewClass extends JFrame {
         setTitle("Maze solver");
         setSize(640,720);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DepthFirst.searchPath(maze,1,1,path);
     }
     @Override
     public void paint(Graphics graphics){
@@ -43,6 +44,14 @@ public class NewClass extends JFrame {
                 graphics.setColor(Color.red);
                 graphics.drawRect(30*j,30*i,30,30);
             }
+        }
+        for (int i = 0; i < path.size() ; i+=2) {
+            int pathx = path.get(i);
+            int pathy = path.get(i+1);
+            System.out.println("x co-ordinate "+pathx);
+            System.out.println("y co-ordinate "+pathy);
+            graphics.setColor(Color.GREEN);
+            graphics.fillRect(30*pathx,30*pathy,30,30);
         }
     } 
 }
